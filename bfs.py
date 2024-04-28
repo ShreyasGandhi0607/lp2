d728275735bbd3ca4ecef6aee188009fc64d6de3
Shreyas <shreyasgandhi673@gmail.com>
2024-04-28 11:27:48 +0530

    lp2

---
+++ bfs.py
@@ -0,0 +1,22 @@
+g = {
+  0 : [1, 2],
+  1 : [0 , 3, 4],
+  2 : [0, 3],
+  3 : [1, 2, 4],
+  4 : [3 ,1]
+}
+
+def bfs(g , s):
+  q = [s]
+  vis = [s]
+  while q:
+    curr = q.pop(0)
+    print(curr)
+    for c in g[curr]:
+      if c not in  vis:
+        vis.append(c)
+        q.append(c)
+
+
+
+bfs(g , 0)
---
+++ djakstra.py
@@ -0,0 +1,25 @@
+import heapq
+
+def djakstra(graph, start, end):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    pq = [(0, start)]
+    while pq:
+        curr_dist, curr_node = heapq.heappop(pq)
+        if curr_dist > distances[curr_node]:
+            continue
+        for neighbor, weight in graph[curr_node].items():
+            distance = curr_dist + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+    return distances[end]
+
+graph = {
+    'A': {'B': 1, 'C': 4},
+    'B': {'A': 1, 'C': 2, 'D': 5},
+    'C': {'A': 4, 'B': 2, 'D': 1},
+    'D': {'B': 5, 'C': 1}
+}
+
+print(djakstra(graph, 'A', 'D'))
